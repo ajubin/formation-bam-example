950f39626e4abee50d844cf085a03374517a221d
Antoine Jubin <antoine.jubin@gmail.com>
2018-12-11 10:10:21 +0100

    page: add Home page with welcome view

---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+// @flow
+
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "white",
+    flex: 1
+  },
+  welcomeContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  welcomeTitle: {
+    color: "#49A698",
+    fontSize: 30
+  },
+  spacer: {
+    flex: 2
+  },
+  bodyText: {}
+});
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.welcomeContainer}>
+          <Text style={styles.welcomeTitle}>Bienvenue</Text>
+        </View>
+        <View style={styles.spacer}>
+          <Text>Michel</Text>
+        </View>
+      </View>
+    );
+  }
+}
